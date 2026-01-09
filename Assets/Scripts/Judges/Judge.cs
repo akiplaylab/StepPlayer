97c8950e26fa8a6ddede6a23d9eeb72fb7c8fa4d
AkiPlayLab <aki.grosjean@gmail.com>
2026-01-10 02:40:07 +0900

    Split JudgementOutcome into its own file

--- Assets/Scripts/Judges/Judge.cs
+++ Assets/Scripts/Judges/Judge.cs
@@ -49,17 +49,3 @@ public sealed class Judge
         return new JudgementOutcome(judgement, intensity, dt <= miss);
     }
 }
-
-public readonly struct JudgementOutcome
-{
-    public JudgementOutcome(Judgement judgement, float intensity, bool shouldConsumeNote)
-    {
-        Judgement = judgement;
-        Intensity = intensity;
-        ShouldConsumeNote = shouldConsumeNote;
-    }
-
-    public Judgement Judgement { get; }
-    public float Intensity { get; }
-    public bool ShouldConsumeNote { get; }
-}
